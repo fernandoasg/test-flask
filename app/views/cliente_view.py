3b69ab46059da3a6e4ce5db5d975eb7a47a6391f
fernandoasg <f3rnand0_lp@hotmail.com>
2019-08-12 22:29:45 -0300

    Inserindo Clientes

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app.forms import cliente_form
+from app import db
 
 from app import app
 
@@ -11,8 +12,26 @@ from app import app
 # @app.route("/oi")
 # def oi():
 #     return "Oi, mundo!"
-@app.route("/cadastrar_cliente")
+from app.models import cliente_model
+
+
+@app.route("/cadastrar_cliente", methods=["GET", "POST"])
 def cadastrar_cliente():
     form = cliente_form.ClienteForm()
+
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        data_nascimento = form.data_nascimento.data
+        profissao = form.profissao.data
+        sexo = form.sexo.data
+
+        cliente = cliente_model.Cliente(nome=nome, email = email, data_nascimento=data_nascimento, profissao=profissao, sexo=sexo)
+
+        try:
+            db.session.add(cliente)
+            db.session.commit()
+        except:
+            print("Cliente não cadastrado")
 
     return render_template("clientes/form.html", form=form)
