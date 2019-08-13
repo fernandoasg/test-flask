5683afe21379c247dc2b0bb71458c4e4dde0272e
fernandoasg <f3rnand0_lp@hotmail.com>
2019-08-12 21:01:30 -0300

    First commit

---
+++ app/views/cliente_view.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+
+@app.route("/ola", defaults={'nome':None}, methods={"POST", "GET"})
+@app.route("/ola/<string:nome>")
+def teste(nome):
+    return render_template("clientes/teste.html", nome_usuario=nome)
+
+@app.route("/oi")
+def oi():
+    return "Oi, mundo!"
